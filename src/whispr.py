1ae0195c3536ea63e2d89c876fa3d7c68229e5ed
ExxML <andywu9509@gmail.com>
2025-10-28 17:26:17 -0700

    Removed Ctrl + C to exit

--- src/whispr.py
+++ src/whispr.py
@@ -1,8 +1,6 @@
-from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import QApplication
 from ui.overlay_window import Overlay
 import sys
-import signal
 import json
 import os
 
@@ -14,12 +12,5 @@ if __name__ == '__main__':
     
     app = QApplication(sys.argv)
     overlay = Overlay()
-    
-    ########## ########## ########## Allows Ctrl + C in terminal to exit ########## ########## ##########
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    timer = QTimer()
-    timer.timeout.connect(lambda: None)  # Do nothing, just wake up
-    timer.start(100)  # Check every 100ms
-    ########## ########## ########## ########## ########## ########## ########## ########## ####
     
     sys.exit(app.exec())
